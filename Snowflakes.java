1235b53de7c8423275d541c16f49da2fece760c2
QingDengYinShan <17610591305@163.com>
2026-09-13 19:20:30 +0800

    Modify mount method to adjust snowflake pattern

--- Snowflakes.java
+++ Snowflakes.java
@@ -2,15 +2,17 @@ public class Snowflakes{
 	static void mount(int x){
 		int i = 0, n;
 		n = x;
-		while(i < n){
+		while(i <= n){
 			System.out.println("*".repeat(i));
 			i++;
 		}
+		
+		i = i - 2;
 
-		while(n > 0){
-			System.out.println("*".repeat(n));
-			n--;
+		while(i > 0){
+			System.out.println("*".repeat(i));
+			i--;
 		}
 	}
 }
-		
+	
